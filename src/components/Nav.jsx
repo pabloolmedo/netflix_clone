57eab26aff2718594de58b221c76031d54b43502
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2022-01-04 12:30:35 +1000

    Nav styled

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "../styles/nav.css";
 
 const Nav = () => {
   return (
     <nav className="nav">
       <div className="nav__leftContainer">
         {/* LOGO */}
-        <img className="nav__logo" src="" alt="netflix logo" />
+        <img
+          className="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="netflix logo"
+        />
 
         {/* menu */}
         <div className="nav__links">
@@ -21,10 +26,14 @@ const Nav = () => {
         </div>
       </div>
       <div className="nav__rightContainer">
-        {/* user avatar */}
-        <img src="" alt="user avatar" className="nav__avatar" />
         {/* Log out button */}
         <button className="nav__btn--logOut">Log out</button>
+        {/* user avatar */}
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="user avatar"
+          className="nav__avatar"
+        />
       </div>
     </nav>
   );
