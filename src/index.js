55bc5539830ea1bb4f58b623b134b8c1d3eacd7d
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2022-01-18 19:08:00 +1000

    Pages added - React-router-dom package installed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+
 
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -20,7 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
